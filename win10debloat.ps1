961b8af4dd914b825c2ddbb0d5ac1c2403905aa6
Seyd Havens <41532415+sthavens@users.noreply.github.com>
2020-10-11 20:16:47 -0700

    Update win10debloat.ps1
    Updated script syntax in notes

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -12,7 +12,7 @@
 #
 #	Addition: One command to rule them all, One command to find it, and One command to Run it! 
 #
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JJ8R4')"
+#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/sthavens/win10script/master/win10debloat.ps1')"
 #
 #	Chris Titus Additions:
 #
